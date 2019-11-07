389067b52dad703ff2479634fbe7b1cbf941bcf5
kaoru <679719+0x6b@users.noreply.github.com>
2019-11-08 01:30:52 +0900

    (refactor) show welcome message only if initialization succeeds

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -7,10 +7,12 @@ import (
 )
 
 func main() {
+	executor := shell.NewSoracomExecutor("/bin/sh")
+	completer := shell.NewSoracomCompleter("resources/soracom-api.en.yaml")
 	fmt.Println("Type `exit` or `Ctrl-D` to exit.")
 	gp.New(
-		shell.NewSoracomExecutor("/bin/sh").Execute,
-		shell.NewSoracomCompleter("resources/soracom-api.en.yaml").Complete,
+		executor.Execute,
+		completer.Complete,
 		gp.OptionTitle("SORACOM Shell"),
 		gp.OptionPrefix("SORACOM> "),
 		gp.OptionMaxSuggestion(10),
