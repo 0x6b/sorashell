27e7912bd283212a44cf5787c1acff499730d0ba
kaoru <679719+0x6b@users.noreply.github.com>
2019-11-08 14:24:07 +0900

    (feat) display logo on startup

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -9,7 +9,36 @@ import (
 func main() {
 	executor := shell.NewSoracomExecutor("/bin/sh")
 	completer := shell.NewSoracomCompleter("resources/soracom-api.en.yaml")
-	fmt.Println("Type `exit` or `Ctrl-D` to exit.")
+
+	fmt.Print(`
+              ..;;ttLLSSSSSSLLtt;;..
+          ..11SSSSSSSSSSSSSSSSSSSSSS11..
+        ::LLSSSSSSttii::,,::iittSSSSSSLL::
+      ::SSSSSS11..              ..11SSSSSS::
+    ::SSSSSSSSttii::..              ::LLSSSS::
+  ..LLSSSSSSSSSSSSSSSSffii::..        ,,LLSSLL..
+  11SSSS::,,;;ttLLSSSSSSSSSSSSff11::..  ::SSSS11
+..SSSS11          ,,;;11LLSSSSSSSSSSSS..  11SSSS..
+iiSSSS,,                  ..::11LLSSSS..  ,,SSSSii
+ttSSff                          ;;SSSS..    ffSSff
+LLSSii                          ;;SSSS..    iiSSLL
+SSSS;;                        ,,11SSSS..    ;;SSSS
+SSSS::                ,,iittLLSSSSSSSS..    ::SSSS
+SSSS;;      ..::iittSSSSSSSSSSSSSSffii      ;;SSSS
+LLSSii    ;;SSSSSSSSSSSSLLttii,,            iiSSLL
+ttSSff    ..LLSSSStt;;,,          ::        ffSSff
+iiSSSS,,    iiSSSS,,          ,,::tt,,..  ,,SSSSii
+..SSSS11    ..LLSStt          ;;LLSStt..  11SSSS..
+  11SSSS::    iiSSSS,,          LLff;;  ::SSSS11
+  ..LLSSLL,,  ..LLSStt  ..tt11..,,  ::,,LLSSLL..
+    ::SSSSLL::  iiSSSS::ffSSSS;;    ::LLSSSS::
+      ::SSSSSS11,,LLSSSSSSSS11  ..11SSSSSS::
+        ,,LLSSSSSSLLSSSSSSffiittSSSSSSLL::
+          ..11LLSSSSSSSSSSSSSSSSSSLL11..
+              ..;;ttLLSSSSSSLLtt;;..
+
+                      Type exit or Ctrl-D to exit.
+`)
 	gp.New(
 		executor.Execute,
 		completer.Complete,
