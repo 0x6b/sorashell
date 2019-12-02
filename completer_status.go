83db5fe86852338bbe956710689a6c43e5a84322
kaoru <679719+0x6b@users.noreply.github.com>
2019-12-02 22:37:26 +0900

    (fix) remove internal statuses

--- completer_status.go
+++ completer_status.go
@@ -6,9 +6,7 @@ var statusFilterSuggestions = func(word string) []prompt.Suggest {
 	return filterFunc([]prompt.Suggest{
 		{Text: "active", Description: ""},
 		{Text: "inactive", Description: ""},
-		{Text: "instock", Description: ""},
 		{Text: "ready", Description: ""},
-		{Text: "shipped", Description: ""},
 		{Text: "suspended", Description: ""},
 		{Text: "terminated", Description: ""},
 	}, word, prompt.FilterFuzzy)
