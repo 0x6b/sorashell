3802f5fb2858d375acb58816f112d3dbb85ebc64
kaoru <679719+0x6b@users.noreply.github.com>
2019-12-02 23:07:19 +0900

    (fix) add speed classes for KM1 and DU

--- completer_speed_class.go
+++ completer_speed_class.go
@@ -4,10 +4,13 @@ import "github.com/c-bata/go-prompt"
 
 var speedClassFilterSuggestions = func(word string) []prompt.Suggest {
 	return filterFunc([]prompt.Suggest{
-		{Text: "s1.minimum", Description: ""},
-		{Text: "s1.slow", Description: ""},
-		{Text: "s1.standard", Description: ""},
-		{Text: "s1.fast", Description: ""},
-		{Text: "s1.4xfast", Description: ""},
+		{Text: "s1.minimum", Description: "plan01s, plan01s - Low Data Volume, plan-D, plan-K"},
+		{Text: "s1.slow", Description: "plan01s, plan01s - Low Data Volume, plan-D, plan-K"},
+		{Text: "s1.standard", Description: "plan01s, plan01s - Low Data Volume, plan-D, plan-K"},
+		{Text: "s1.fast", Description: "plan01s, plan01s - Low Data Volume, plan-D, plan-K"},
+		{Text: "s1.4xfast", Description: "plan01s, plan01s - Low Data Volume"},
+		{Text: "t1.standard", Description: "plan-KM1"},
+		{Text: "u1.slow", Description: "plan-DU"},
+		{Text: "u1.standard", Description: "plan-DU"},
 	}, word, prompt.FilterFuzzy)
 }
